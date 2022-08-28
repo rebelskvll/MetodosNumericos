3077e6f0d4cb71767a4da39ee924ff985df7c600
Rebelskvll <raulemoncadat@gmail.com>
2022-08-28 17:19:41 -0500

    Se agrega el requirements.txt, se modifica el README.md y se agregan funcionalidades

--- MetodoBiseccion.py
+++ MetodoBiseccion.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 """
-Scripts de python para el módulo virtual de métodos numéricos suministrados
-en la lectura fundamental 1, adaptados por Raúl Moncada.
+Script de python del método de bisección para el módulo virtual de métodos
+numéricos suministrado en la lectura fundamental 1, adaptado por Raúl Moncada.
 """
 
 from math import *
+from prettytable import PrettyTable
+
+# Se crea la tabla de forma global
+resultados = PrettyTable(["Iteración","p"])
 
 #Acá se define la función a evaluar, recibe como parámetro X
 def f(x):
@@ -20,16 +24,25 @@ def biseccion(f, punto_a, punto_b, tolerancia, iteraciones_evaluar):
 
     iteraciones=1
     while iteraciones <= iteraciones_evaluar:
+
         p=punto_a+(punto_b-punto_a)/2.0
-        print("Iteración", "%03d" % iteraciones, ": p=", "%.14f" %p)
+        """
+        Se agregan van agregando las filas a la tabla conforme 
+        se hace el cálculo
+        """
+        resultados.add_row(["%03d" % iteraciones,"%.14f" % p])
         if abs(f(p)) <= 1e-15 or (punto_b - punto_a)/2.0 < tolerancia:
             return p
-        iteraciones += 1
+
         if f(punto_a) * f(p) > 0:
             punto_a=p
         else:
             punto_b=p
-    print("Interacciones agotadas: Error")
+
+        iteraciones += 1
+
+    print("Iteraciones finalizadas, no se encontró el resultado")
+
     return
 
 print("\n"+r"Metodo de la Bisección:"+"\n")
@@ -40,4 +53,6 @@ Llama a la función bisección y le pasa como parámetros:
 - La tolerancia a error
 - El número de itereaciones donde se quiere detener el proceso
 """
-biseccion(f,-1.0, -0.25, 1e-4, 10)
+biseccion(f,-1.0, -0.25, 1e-4, 100)
+#Imprime la tabla con los valores calculados
+print (resultados)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # MetodosNumericos
-Repositorio para los scripts suministrados para métodos numéricos del Politécnico Grancolombiano
+Repositorio para los scripts suministrados en el módulo virtual de métodos numéricos del Politécnico Grancolombiano. 
+Estos scripts han sido tomados de la lectura fundamental 1 y se han modificado para hacer más fácil su lectura e interpretación, documentando las funciones, modificando algunos nombres de variables y cambiando la forma en la que se presentan los resultados.
+
+# Requerimientos
+
+Se debe instalar el módulo `prettytable` a través del comando `pip install -r requirements.txt` para que se muestre el resultado en una tabla ascii organizada.
+
+Ejemplo de la salida en pantalla de los resultados.
+
+Metodo de la Bisección:
+
++-----------+-------------------+
+| Iteración |         p         |
++-----------+-------------------+
+|    001    | -0.62500000000000 |
+|    002    | -0.43750000000000 |
+|    003    | -0.53125000000000 |
+|    004    | -0.57812500000000 |
+|    005    | -0.55468750000000 |
+|    006    | -0.54296875000000 |
+|    007    | -0.53710937500000 |
+|    008    | -0.53417968750000 |
+|    009    | -0.53564453125000 |
+|    010    | -0.53491210937500 |
+|    011    | -0.53454589843750 |
+|    012    | -0.53472900390625 |
+|    013    | -0.53482055664062 |
++-----------+-------------------+
